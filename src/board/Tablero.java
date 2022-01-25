9c5ecad6a9314fbe8019bf669064e3ff17a20021
Guzmansrz <guzmansuarezalvarez@gmail.com>
2022-01-25 09:37:27 +0100

    commit

--- src/board/Tablero.java
+++ src/board/Tablero.java
@@ -1,5 +1,16 @@
 package board;
 
 public class Tablero {
+    private static int NUMANIMALES=10;
+    private static int CAZADOR=1;
+    private static int CHUPACABRAS=1;
 
+    private Object[][] tablero;
+    
+	protected void inicializarTablero() {
+		int contAnimales=0;
+		int contCazador=0;
+		int contChupacabras=0;
+		for(int i=0; i<tablero.length )
+	}
 }
